382b4bb9ec17059a594bec37cdf4461d10f5d3c1
betelgeuse06 <32902105+betelgeuse06@users.noreply.github.com>
2019-06-15 23:47:27 +0900

    ㅇㅋ 완성함

---
+++ BrokenStar/Rock Scissors Paper.py
@@ -0,0 +1,19 @@
+import random
+
+a = input("1. 가위, 2. 바위, 3. 보")
+i = randint(1, 3)
+
+if a == i:
+    print("동점")
+elif a == 1 && i == 2:
+    print("졌어 ㅜㅜ")
+elif a == 1 && i == 3:
+    print("이김 ㅊㅋ")
+elif a == 2 && i == 1:
+    print("이김 ㅊㅋ")
+elif a == 2 && i == 3:
+    print("졌어 ㅜㅜ")
+elif a == 3 && i == 1:
+    print("졌어 ㅜㅜ")
+elif a == 3 && i == 2:
+    print("이김 ㅊㅋ")

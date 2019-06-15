a780e975b1e076bbdecb89cb6146c99997e3db39
betelgeuse06 <32902105+betelgeuse06@users.noreply.github.com>
2019-06-15 23:56:34 +0900

    다드음

--- BrokenStar/Rock Scissors Paper.py
+++ BrokenStar/Rock Scissors Paper.py
@@ -1,19 +1,21 @@
 import random
 
-a = input("1. 가위, 2. 바위, 3. 보")
-i = randint(1, 3)
+for i in range(5):
+     num=int(input("0.가위 1.바위 2.보 중 하나를 선택하세요>>\n"))
+     com=random.randrange(1,3)
 
-if a == i:
-    print("동점")
-elif a == 1 && i == 2:
-    print("졌어 ㅜㅜ")
-elif a == 1 && i == 3:
-    print("이김 ㅊㅋ")
-elif a == 2 && i == 1:
-    print("이김 ㅊㅋ")
-elif a == 2 && i == 3:
-    print("졌어 ㅜㅜ")
-elif a == 3 && i == 1:
-    print("졌어 ㅜㅜ")
-elif a == 3 && i == 2:
-    print("이김 ㅊㅋ")
+     if num>2:
+          print("잘못 누르셨습니다")
+     elif num==com:
+          print("비겼습니다")
+     elif num+1==com or (num==2 and com==0):
+          print("컴퓨터 승리")
+     else:
+          print("내가 승리")
+
+     #내가 가위내면(num=0)-> com=1(진다),com=2(이긴다)
+     #내가 바위내면 (num=1)-> com=0(이긴다), com= 2(진다)
+     #내가 보 내면(num=2) -> com=1(이긴다),com=0(진다)
+
+com=random.randrange(1,3)
+print(com)
